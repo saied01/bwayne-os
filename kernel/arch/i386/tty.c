e61a73056038e6530b85b358f357089cb069ae82
saied <rama.saied11@gmail.com>
2026-02-11 19:25:20 -0300

    *

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -21,6 +21,8 @@ void terminal_initialize(void)
   terminal_row = 0;
   terminal_column = 0;
   terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+  outport_b(0x3D4, 0x0A);
+  outport_b(0x3D5, 0x20);
 
   for (size_t y = 0; y < VGA_HEIGHT; y++)
   {
